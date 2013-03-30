44c697371377ba687905c1d1a2fdc3e7ba19a5d2
ahmed elgendy <a7medelgendy@gmail.com>
2013-03-30 12:56:18 +0200

    learn git add folder remote

---
+++ application/logs/log-2013-02-23.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-02-23 23:04:06 --> Query error: No database selected
+ERROR - 2013-02-23 23:04:06 --> Language file contains no data: language/english/db_lang
+ERROR - 2013-02-23 23:04:06 --> Could not find the language line "db_error_heading"
+ERROR - 2013-02-23 23:05:15 --> Unable to connect to the database
+ERROR - 2013-02-23 23:13:48 --> Unable to connect to the database
+ERROR - 2013-02-23 23:14:58 --> Unable to connect to the database
+ERROR - 2013-02-23 23:16:23 --> Query error: No database selected
+ERROR - 2013-02-23 23:16:23 --> Language file contains no data: language/english/db_lang
+ERROR - 2013-02-23 23:16:23 --> Could not find the language line "db_error_heading"
+ERROR - 2013-02-23 23:27:17 --> Severity: Notice  --> Undefined index: id_menu C:\wamp\www\ionize\application\controllers\admin\article.php 1861
+ERROR - 2013-02-23 23:27:17 --> Severity: Notice  --> Undefined index: name C:\wamp\www\ionize\application\controllers\admin\article.php 1881
---
+++ application/logs/log-2013-03-13.php
@@ -0,0 +1,4 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2013-03-13 10:05:07 --> Unable to select database: ionize
+ERROR - 2013-03-13 10:11:29 --> Unable to select database: ionize
